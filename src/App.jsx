31be38ddf28740e35c273d2a32aaf378adef4d3a
Joginder Kumar <joginder.director@gmail.com>
2026-03-17 16:29:39 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div className='flex justify-center item-center'>
+      <h1>School management</h1>
+    </div>
+  )
+}
+
+export default App
